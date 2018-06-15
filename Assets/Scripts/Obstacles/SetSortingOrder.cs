2345b2482c7e3560d5a802510899c1eb89fe773d
Tojikan <andrew.eqian@gmail.com>
2018-06-15 16:15:34 -0400

    Sorting Order and 2 types of falling object
    Some additional stuff

---
+++ Assets/Scripts/Obstacles/SetSortingOrder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Sets the sorting order for sprites after it drops behind the y value of the player object
+/// </summary>
+public class SetSortingOrder : MonoBehaviour
+{
+    private static float playerY;
+    private bool isSorted;
+
+    //find the player if it hasn't been found yet. 
+    private void Awake()
+    {
+        if (playerY == 0)
+        {
+            GameObject player = FindObjectOfType<PlayerController>().gameObject;
+            playerY = player.transform.position.y + player.GetComponent<Collider2D>().offset.y;
+        }
+        isSorted = false;
+    }
+
+    private void Update()
+    {
+        if (!isSorted)
+        {
+            if (gameObject.transform.position.y > playerY)
+            {
+                gameObject.GetComponent<Obstacle>().SetLayer(5);
+                isSorted = false;
+            }
+        }
+    }
+}
